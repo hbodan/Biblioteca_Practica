b54f5b10af7342a15f2daf7fffa94db21c738c45
franygutz <franyagl@uamv.edu.ni>
2025-10-15 18:03:00 -0600

    JPA y entities by Franya

---
+++ src/main/java/entities/Libro.java
@@ -0,0 +1,30 @@
+package entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table
+@Getter @Setter
+public class Libro {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "titulo", length = 100, nullable = false)
+    private String titulo;
+
+    @Column(name = "anio_publicacion", length = 100, nullable = false)
+    private String anioPublicacion;
+
+    @ManyToOne
+    @JoinColumn(name = "autor")
+    private Autor autor;
+
+    @ManyToMany
+    @JoinTable(name = "libro_categoria", joinColumns = @JoinColumn(name = "libro_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+    private List<Categoria> categorias;
+}
